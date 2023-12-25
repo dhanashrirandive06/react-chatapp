4589a98c9c1f67c63228f81f97423ad0dec250b9
dhanashrirandive06 <dhanashrirandive06.com>
2023-12-25 16:27:48 +0530

    version6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 // ******** Imports ******** //
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
 
 function App() {
   const [notify, setNotify] = useState(null);
@@ -17,29 +12,31 @@ function App() {
     setNotify(message);
   };
 
-  const routes = createRoutesFromElements(
-    <>
-      <Route
-        path="/"
-        element={
-          <Login notify={notify} handleNotification={handleNotification} />
-        }
-      />
-      <Route
-        path="/dashboard"
-        element={
-          <Dashboard notify={notify} handleNotification={handleNotification} />
-        }
-      />
-    </>
-  );
-
-  const router = createBrowserRouter(routes);
-
   return (
     <>
       <div className="main">
-        <RouterProvider router={router} />
+        <BrowserRouter basename="/react-chatapp">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Login
+                  notify={notify}
+                  handleNotification={handleNotification}
+                />
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <Dashboard
+                  notify={notify}
+                  handleNotification={handleNotification}
+                />
+              }
+            />
+          </Routes>
+        </BrowserRouter>
       </div>
       <ToastContainer />
     </>
